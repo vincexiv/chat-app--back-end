9e5061911cff03ee41ca0a5c3cf53d04e90dec75
Vincent Makokha <makokhavomondi@gmail.com>
2022-12-02 22:01:43 +0300

    Implement login and logout functionalities
    Authentication has been baked in these processes through the use of session

--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,7 @@ Rails.application.routes.draw do
   # root "articles#index"
   post '/signup', to: 'users#create'
   get '/me', to: 'users#show'
+
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
 end
