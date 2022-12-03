1c27179825801f100698b688df02b537edcedb33
Vincent Makokha <makokhavomondi@gmail.com>
2022-12-03 20:43:03 +0300

    Use manual usernames and passwords for seed file
    Would be better when testing how the site would look from various user's perspective

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,19 +10,56 @@ puts "🌱 seeding"
 
 puts "users..."
 users = []
-10.times do
-    password = Faker::Lorem.characters(number: rand(5..15))
 
-    users << User.create(
-        full_name: Faker::Name.name,
-        email: Faker::Internet.email,
-        username: Faker::Internet.username(specifier: 5..10),
-        password: password,
-        password_confirmation: password,
+users << User.create(
+        full_name: "Vincent Makokha",
+        email: "vincent@example.com",
+        username: "vincent",
+        password: "vincent",
+        password_confirmation: "vincent",
         bio: Faker::Lorem.paragraph(sentence_count: rand(1..5)),
         profile_picture: Faker::Avatar.image
     )
-end
+
+users << User.create(
+        full_name: "Rajab Karume",
+        email: "rajab@example.com",
+        username: "rajab",
+        password: "rajab",
+        password_confirmation: "rajab",
+        bio: Faker::Lorem.paragraph(sentence_count: rand(1..5)),
+        profile_picture: Faker::Avatar.image
+    )
+
+users << User.create(
+        full_name: "Mercy Cheruiyot",
+        email: "mercy@example.com",
+        username: "mercy",
+        password: "mercy",
+        password_confirmation: "mercy",
+        bio: Faker::Lorem.paragraph(sentence_count: rand(1..5)),
+        profile_picture: Faker::Avatar.image
+    )
+
+users << User.create(
+        full_name: "Zablon Nyabuto",
+        email: "zablon@example.com",
+        username: "zablon",
+        password: "zablon",
+        password_confirmation: "zablon",
+        bio: Faker::Lorem.paragraph(sentence_count: rand(1..5)),
+        profile_picture: Faker::Avatar.image
+    )
+
+users << User.create(
+        full_name: "Andrew Ochieng",
+        email: "andrew@example.com",
+        username: "andrew",
+        password: "andrew",
+        password_confirmation: "andrew",
+        bio: Faker::Lorem.paragraph(sentence_count: rand(1..5)),
+        profile_picture: Faker::Avatar.image
+    )
 
 puts "messages..."
 messages = []
