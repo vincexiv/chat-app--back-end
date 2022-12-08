1e00164e6b96fc069a70d6ac6bd6e4868960667a
Vincent Makokha <makokhavomondi@gmail.com>
2022-12-08 09:22:22 +0300

    Solve a bug I introduced. Use sender (which exists) in stead of sender_id

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -30,7 +30,7 @@ class MessagesController < ApplicationController
 
     private
     def message_params
-        params.permit(:content, :sender_id, :receiver_id)
+        params.permit(:content, :sender, :receiver)
     end
 
     def record_not_found
